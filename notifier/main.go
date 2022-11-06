8ecf7a5b83ce941b55e649bd5fb260607e40138d
Alishan Ladhani <git@alad.ca>
2022-11-06 16:53:59 -0500

    Refactor

--- notifier/main.go
+++ notifier/main.go
@@ -13,27 +13,36 @@ import (
 func main() {
 	status, err := status.LoadFrom("status.json")
 	if err != nil {
-		log.Fatalln(fmt.Errorf("could not load status: %w", err))
+		log.Fatalf("Could not load status file: %s", err)
 	}
 
 	cfg, err := config.FromEnv()
 	if err != nil {
-		log.Fatalln(fmt.Errorf("could not load config: %w", err))
+		log.Fatalf("Could not load config: %s", err)
 	}
 
 	log.Printf("Running notifier for booking times %s to %s with preferred stations %s\n", status.BookingStart, status.BookingEnd, cfg.PreferredStations)
 
+	err = notifyForPreferredStations(status, cfg)
+	if err != nil {
+		log.Fatalf("Could not notify for preferred stations: %s", err)
+	}
+}
+
+func notifyForPreferredStations(status *status.Status, cfg *config.Config) error {
 	for _, station := range status.Stations {
-		if station.HasAvailableCars() && containsAny(station.Name, cfg.PreferredStations) {
+		if containsAny(station.Name, cfg.PreferredStations) && station.HasAvailableCars() {
 			message := fmt.Sprintf("Car(s) available at %s from %s to %s", station.Name, status.BookingStart, status.BookingEnd)
 			log.Println(message)
 
-			err = webhook.Send(cfg.WebhookURL, message)
+			err := webhook.Send(cfg.WebhookURL, message)
 			if err != nil {
-				log.Fatalln(fmt.Errorf("could not send webhook: %w", err))
+				return fmt.Errorf("could not send webhook: %w", err)
 			}
 		}
 	}
+
+	return nil
 }
 
 func containsAny(stationName string, preferredStations []string) bool {
