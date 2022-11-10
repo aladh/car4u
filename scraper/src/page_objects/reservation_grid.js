42e4eb979cb807dbb5dc67d35163946ed8b61d53
Alishan Ladhani <8869764+aladh@users.noreply.github.com>
2022-11-10 03:34:29 +0000

    Refactor

--- scraper/src/page_objects/reservation_grid.js
+++ scraper/src/page_objects/reservation_grid.js
@@ -8,28 +8,19 @@ export default class ReservationGrid {
     const stationDivs = await this.page.$$('.tblAv')
 
     for (const stationDiv of stationDivs) {
-      const station = {}
+      const station = {
+        name: await stationDiv.$eval('.strongName a', e => e.textContent),
+        cars: []
+      }
 
-      const stationNameA = await stationDiv.$('.strongName a')
-      station.name = await this.page.evaluate(e => e.textContent, stationNameA)
-
-      const cars = []
       const carDivs = await stationDiv.$$('.bRow')
 
       for (const carDiv of carDivs) {
-        const car = {}
-
-        const carNameDiv = await carDiv.$('.divDesc')
-        car.name = await this.page.evaluate(e => e.innerText, carNameDiv)
-
-        const carAvailableContainer = await carDiv.$('.cBCar strong')
-        const carAvailableContainerText = await this.page.evaluate(e => e.innerText, carAvailableContainer)
-        car.available = carAvailableContainerText === 'Select'
-
-        cars.push(car)
+        station.cars.push({
+          name: await carDiv.$eval('.divDesc', e => e.innerText),
+          available: await carDiv.$eval('.cBCar strong', e => e.innerText) === 'Select'
+        })
       }
-
-      station.cars = cars
 
       stations.push(station)
     }
