cde3f626b5104b8804e4cd50cb550a1a2c650bd5
Alishan Ladhani <git@alad.ca>
2023-10-12 22:45:58 -0400

    Refactor selectors into an object to avoid redundant suffix

--- scraper/src/page_objects/booking_page.js
+++ scraper/src/page_objects/booking_page.js
@@ -1,18 +1,17 @@
 import ReservationGrid from './reservation_grid.js'
 
-const START_YEAR_SELECTOR = 'input[name=StartYear]'
-const START_MONTH_SELECTOR = 'input[name=StartMonth]'
-const START_DAY_SELECTOR = 'input[name=StartDay]'
-const START_HOUR_SELECTOR = 'select[name=StartHour]'
-const START_MINUTE_SELECTOR = 'select[name=StartMinute]'
-const END_YEAR_SELECTOR = 'input[name=EndYear]'
-const END_MONTH_SELECTOR = 'input[name=EndMonth]'
-const END_DAY_SELECTOR = 'input[name=EndDay]'
-const END_HOUR_SELECTOR = 'select[name=EndHour]'
-const END_MINUTE_SELECTOR = 'select[name=EndMinute]'
-const SEARCH_BUTTON_SELECTOR = '#Button_Disponibility'
-
 const SELECTORS = {
+  startYear: 'input[name=StartYear]',
+  startMonth: 'input[name=StartMonth]',
+  startDay: 'input[name=StartDay]',
+  startHour: 'select[name=StartHour]',
+  startMinute: 'select[name=StartMinute]',
+  endYear: 'input[name=EndYear]',
+  endMonth: 'input[name=EndMonth]',
+  endDay: 'input[name=EndDay]',
+  endHour: 'select[name=EndHour]',
+  endMinute: 'select[name=EndMinute]',
+  searchButton: '#Button_Disponibility',
   stationDropdown: '#ddcl-StationID',
   allStationsCheckbox: '#ddcl-StationID-i1',
 }
@@ -25,27 +24,27 @@ export default class BookingPage {
   // Navigate to the iframe that contains the reservation grid
   async goto() {
     await this.page.goto('https://www.reservauto.net/Scripts/Client/ReservationAdd.asp?ReactIframe=true&CurrentLanguageID=2')
-    await this.page.waitForSelector(START_MONTH_SELECTOR)
+    await this.page.waitForSelector(SELECTORS.startMonth)
   }
 
   async search({ startYear, startMonth, startDay, startHour, startMinute, endYear, endMonth, endDay, endHour, endMinute }) {
-    await this.page.$eval(START_YEAR_SELECTOR, (el, value) => el.value = value, startYear)
-    await this.page.$eval(START_MONTH_SELECTOR, (el, value) => el.value = value, startMonth)
-    await this.page.$eval(START_DAY_SELECTOR, (el, value) => el.value = value, startDay)
-    await this.page.select(START_HOUR_SELECTOR, startHour)
-    await this.page.select(START_MINUTE_SELECTOR, startMinute)
+    await this.page.$eval(SELECTORS.startYear, (el, value) => el.value = value, startYear)
+    await this.page.$eval(SELECTORS.startMonth, (el, value) => el.value = value, startMonth)
+    await this.page.$eval(SELECTORS.startDay, (el, value) => el.value = value, startDay)
+    await this.page.select(SELECTORS.startHour, startHour)
+    await this.page.select(SELECTORS.startMinute, startMinute)
 
-    await this.page.$eval(END_YEAR_SELECTOR, (el, value) => el.value = value, endYear)
-    await this.page.$eval(END_MONTH_SELECTOR, (el, value) => el.value = value, endMonth)
-    await this.page.$eval(END_DAY_SELECTOR, (el, value) => el.value = value, endDay)
-    await this.page.select(END_HOUR_SELECTOR, endHour)
-    await this.page.select(END_MINUTE_SELECTOR, endMinute)
+    await this.page.$eval(SELECTORS.endYear, (el, value) => el.value = value, endYear)
+    await this.page.$eval(SELECTORS.endMonth, (el, value) => el.value = value, endMonth)
+    await this.page.$eval(SELECTORS.endDay, (el, value) => el.value = value, endDay)
+    await this.page.select(SELECTORS.endHour, endHour)
+    await this.page.select(SELECTORS.endMinute, endMinute)
 
     // Select option to show all stations on reservation grid
     await this.page.click(SELECTORS.stationDropdown)
     await this.page.click(SELECTORS.allStationsCheckbox)
 
-    await this.page.click(SEARCH_BUTTON_SELECTOR)
+    await this.page.click(SELECTORS.searchButton)
     await new Promise(r => setTimeout(r, 2000))
 
     return new ReservationGrid(this.page)
