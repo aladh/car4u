6e7819335f8f9bb60c07b04ee96f8f3a82afb6df
Alishan Ladhani <git@alad.ca>
2023-11-10 14:11:47 -0500

    Improve formatting of notification

--- car4u/src/main/kotlin/Main.kt
+++ car4u/src/main/kotlin/Main.kt
@@ -39,7 +39,8 @@ suspend fun main() {
     }.forEach { station ->
       sendWebhook(
         WEBHOOK_URL,
-        "Date(${reservationGrid.bookingStart}-${reservationGrid.bookingEnd}) Station(${station.name}) Cars(${station.cars.joinToString { it.name }}})"
+        "**Date**: ${reservationGrid.bookingStart} - ${reservationGrid.bookingEnd}\n**Station**: ${station.name}\n" +
+          "**Cars**: ${station.cars.joinToString { it.name }}"
       )
     }
 }
