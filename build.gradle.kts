e5317f026fb079c1d041a73aac3d03346123c133
Alishan Ladhani <aladhani@gitlab.com>
2023-12-03 20:57:31 -0500

    Replace v1 with v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   kotlin("jvm") version "1.9.21"
+  kotlin("plugin.serialization") version "1.9.21"
   application
   id("io.gitlab.arturbosch.detekt") version "1.23.4"
 }
@@ -27,6 +28,9 @@ dependencies {
   implementation("io.ktor:ktor-client-cio:$ktorVersion")
   implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
   implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+  // JSON serialization
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
   // GitLab API client
   implementation("org.gitlab4j:gitlab4j-api:5.4.0")
